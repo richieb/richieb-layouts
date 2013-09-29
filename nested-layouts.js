6ea8c497a2b1988ea248029aec7bf4d5a2af02a1
Richard <richardofyork@gmail.com>
2013-09-28 21:59:06 -0400

    Update nested-layouts.js
    This is what I am trying to do

--- nested-layouts.js
+++ nested-layouts.js
@@ -5,6 +5,42 @@ if (Meteor.isClient) {
     });
 
     this.route('dynamo', {
+      path: '/dashboard/dynamo',
+        controller:"DashboardController",
+        yieldTemplates: {
+            'dynamo': {to: 'dashboard-dynamic'}
+        }
+    });
+  });
+
+  DashboardController = RouteController.extend({
+    template: 'dashboard',
+      run:function ()  {
+          this.render();
+      }
+  });
+
+ /* DynamoController = DashboardController.extend({
+    run: function () {
+      this.render();
+
+      this.render({
+        dynamo: {to: 'dashboard-dynamic'}
+      });
+    }
+  });*/
+}
+
+
+
+/* 
+if (Meteor.isClient) {
+  Router.map(function () {
+    this.route('dashboard', {
+      path: '/dashboard'
+    });
+
+    this.route('dynamo', {
       path: '/dashboard/dynamo'
     });
   });
@@ -23,3 +59,5 @@ if (Meteor.isClient) {
     }
   });
 }
+
+*/
